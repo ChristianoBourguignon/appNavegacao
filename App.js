ebbdcdda890eee8a2c724fd8661617c9ca2b2a46
Christiano Bourguignon <christianombl@gmail.com>
2023-07-15 16:54:23 -0300

    Usando API DrawerNavigator

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Contacts from './src/pages/Contacts/';
 import Information from './src/pages/Information/';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Stack.Navigator>
         <Stack.Screen name="Contacts" component={Contacts} />
         <Stack.Screen name="Information" component={Information} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Drawer.Navigator>
+        <Drawer.Screen name="Contacts" component={Contacts} />
+        <Drawer.Screen name="Information" component={Information} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
