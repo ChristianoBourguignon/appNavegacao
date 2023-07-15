ead3f34b9f075f0add525085acf393b39cb97bd5
Christiano Bourguignon <christianombl@gmail.com>
2023-07-15 19:10:28 -0300

    Melhoria mais clara para as informações e correção de erro de troca de pagina

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,35 +1,92 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation  } from '@react-navigation/native';
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function Contacts({navigation}) {
+    const titleNavigation = useNavigation();
+    titleNavigation.setOptions({title: 'Contatos'});
+
 
   return (
-    <View>
+    <ScrollView>
         <View style={styles.containerInfo}>
             <Text>Nome: Christiano</Text>
-            <Text>Idade: 22</Text>
+            <Text>Idade: 21</Text>
             <Text>Profissão: Assistente de E-commerce</Text>
-            <TouchableOpacity onPress={()=>navigation.navigate('Information', {info:'christiano'})} style={styles.buttonInfo}>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', 
+            {
+            nome: 'Christiano',
+            idade: '21',
+            profissao: 'Assistente de E-commerce',
+            endereco: 'Rua Sem Saida, 09',
+            numero: '21 99999-9999',
+            hobby: 'Programar'
+            }
+            )} style={styles.buttonInfo}>
                 <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
             </TouchableOpacity>
         </View>
         <View style={styles.containerInfo}>
             <Text>Nome: Pedro</Text>
-            <Text>Idade: 25</Text>
+            <Text>Idade: 24</Text>
             <Text>Profissão: Padeiro</Text>
-            <TouchableOpacity onPress={()=>navigation.navigate('Information', {info:'pedro'})} style={styles.buttonInfo}>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', {
+                nome: 'Pedro',
+                idade: '24',
+                profissao: 'Padeiro',
+                endereco: 'Rua Com Saida, 591',
+                numero: '21 99999-9999',
+                hobby: 'Exercitar-se'
+            })} style={styles.buttonInfo}>
                 <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
             </TouchableOpacity>
         </View>
         <View style={styles.containerInfo}>
             <Text>Nome: Patrick</Text>
-            <Text>Idade: 30</Text>
+            <Text>Idade: 27</Text>
             <Text>Profissão: Padre</Text>
-            <TouchableOpacity onPress={()=>navigation.navigate('Information', {info:'patrick'})} style={styles.buttonInfo}>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', {
+                 nome: 'Patrick',
+                 idade: '27',
+                 profissao: 'Padre',
+                 endereco: 'Rua Almeida Torres, 23',
+                 numero: '21 99999-9999',
+                 hobby: 'Ler Livros'
+            })} style={styles.buttonInfo}>
                 <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
             </TouchableOpacity>
         </View>
-    </View>
+        <View style={styles.containerInfo}>
+            <Text>Nome: Cristiane</Text>
+            <Text>Idade: 46</Text>
+            <Text>Profissão: Professora</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', {
+                nome: 'Cristiane',
+                idade: '46',
+                profissao: 'Professora',
+                endereco: 'Rua Taciana Costa, 23',
+                numero: '21 99999-9999',
+                hobby: 'Ensinar'
+            })} style={styles.buttonInfo}>
+                <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.containerInfo}>
+            <Text>Nome: Alexandre</Text>
+            <Text>Idade: 50</Text>
+            <Text>Profissão: Uber</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', {
+                nome: 'Alexandre',
+                idade: '50',
+                profissao: 'Uber',
+                endereco: 'Rua Alfredo Batista, 56',
+                numero: '21 99999-9999',
+                hobby: 'Ouvir Musica'
+            })} style={styles.buttonInfo}>
+                <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
+            </TouchableOpacity>
+        </View>
+    </ScrollView>
   );
 }
 
@@ -39,6 +96,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         textAlign: 'center',
+        marginBottom: 10,
     },
     txtButtonInfo: {
         fontWeight: 900,
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,44 +1,21 @@
 import React from 'react';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation  } from '@react-navigation/native';
 import { Text, View, StyleSheet } from 'react-native';
 
-export default function Information() {
-    const route = useRoute();
-    const { info } = route.params;
-    
+export default function Information( {route} ) {
+    const titleNavigation = useNavigation();
+    titleNavigation.setOptions({title: 'Informações de ' + route.params?.nome});
 
   return (
     <View style={styles.containerApp}>
-        { info === 'christiano' ? (
-           <View style={styles.containerInfo}>
-            <Text>Nome: <Text style={styles.txtInfo}>Christiano</Text></Text>
-            <Text>Idade: <Text style={styles.txtInfo}>22</Text></Text>
-            <Text>Profissão: <Text style={styles.txtInfo}>Assistente de E-commerce</Text></Text>
-            <Text>Endereço: <Text style={styles.txtInfo}>Rua Sem Saida, 09</Text></Text>
-            <Text>Numero: <Text style={styles.txtInfo}>21 99999-9999</Text></Text>
-            <Text>Hobby: <Text style={styles.txtInfo}>Programar</Text></Text>
-            </View>
-        ):(<View/>)}
-        { info === 'pedro' ? (
-           <View style={styles.containerInfo}>
-            <Text>Nome: <Text style={styles.txtInfo}>Pedro</Text></Text>
-            <Text>Idade: <Text style={styles.txtInfo}>25</Text></Text>
-            <Text>Profissão: <Text style={styles.txtInfo}>Padeiro</Text></Text>
-            <Text>Endereço: <Text style={styles.txtInfo}>Rua Com Saida, 591</Text></Text>
-            <Text>Numero: <Text style={styles.txtInfo}>21 99999-9999</Text></Text>
-            <Text>Hobby: <Text style={styles.txtInfo}>Exercitar-se</Text></Text>
-            </View>
-        ):(<View/>)}
-        { info === 'patrick' ? (
-           <View style={styles.containerInfo}>
-            <Text>Nome: <Text style={styles.txtInfo}>Patrick</Text></Text>
-            <Text>Idade: <Text style={styles.txtInfo}>30</Text></Text>
-            <Text>Profissão: <Text style={styles.txtInfo}>Padre</Text></Text>
-            <Text>Endereço: <Text style={styles.txtInfo}>Rua Almeida Torres, 23</Text></Text>
-            <Text>Numero: <Text style={styles.txtInfo}>21 99999-9999</Text></Text>
-            <Text>Hobby: <Text style={styles.txtInfo}>Ler Livros</Text></Text>
-            </View>
-        ):(<View/>)}
+        <View style={styles.containerInfo}>
+            <Text>Nome: <Text style={styles.txtInfo}>{route.params?.nome}</Text></Text>
+            <Text>Idade: <Text style={styles.txtInfo}>{route.params?.idade}</Text></Text>
+            <Text>Profissão: <Text style={styles.txtInfo}>{route.params?.profissao}</Text></Text>
+            <Text>Endereço: <Text style={styles.txtInfo}>{route.params?.endereco}</Text></Text>
+            <Text>Numero: <Text style={styles.txtInfo}>{route.params?.numero}</Text></Text>
+            <Text>Hobby: <Text style={styles.txtInfo}>{route.params?.hobby}</Text></Text>
+        </View>
     </View>
   );
 }
