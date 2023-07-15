8c51660479bf1735d4e6ae8958b1beef8302e902
Christiano Bourguignon <christianombl@gmail.com>
2023-07-15 18:21:31 -0300

    Mais da API Drawer e usei a useRoute (c ajuda IA)

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,11 +1,57 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Contacts({navigation}) {
+
   return (
     <View>
-        <Text>Christiano</Text>
-        <Text onPress={()=>navigation.navigate('Information')}>Informações do Christiano</Text>
+        <View style={styles.containerInfo}>
+            <Text>Nome: Christiano</Text>
+            <Text>Idade: 22</Text>
+            <Text>Profissão: Assistente de E-commerce</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', {info:'christiano'})} style={styles.buttonInfo}>
+                <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.containerInfo}>
+            <Text>Nome: Pedro</Text>
+            <Text>Idade: 25</Text>
+            <Text>Profissão: Padeiro</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', {info:'pedro'})} style={styles.buttonInfo}>
+                <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.containerInfo}>
+            <Text>Nome: Patrick</Text>
+            <Text>Idade: 30</Text>
+            <Text>Profissão: Padre</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate('Information', {info:'patrick'})} style={styles.buttonInfo}>
+                <Text style={styles.txtButtonInfo}>Mais Informações...</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    containerInfo: {
+        marginTop: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    txtButtonInfo: {
+        fontWeight: 900,
+        textAlign: 'center',
+    },
+    buttonInfo: {
+        padding: 20,
+        backgroundColor: '#fc06',
+        width: '50%',
+        alignContent: 'center',
+        textAlign: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+        borderRadius: 20,
+    }
+})
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Information() {
+    const route = useRoute();
+    const { info } = route.params;
+    
+
   return (
-    <View>
-        <Text>
-        Conteúdo do componente Information
-        </Text>
+    <View style={styles.containerApp}>
+        { info === 'christiano' ? (
+           <View style={styles.containerInfo}>
+            <Text>Nome: <Text style={styles.txtInfo}>Christiano</Text></Text>
+            <Text>Idade: <Text style={styles.txtInfo}>22</Text></Text>
+            <Text>Profissão: <Text style={styles.txtInfo}>Assistente de E-commerce</Text></Text>
+            <Text>Endereço: <Text style={styles.txtInfo}>Rua Sem Saida, 09</Text></Text>
+            <Text>Numero: <Text style={styles.txtInfo}>21 99999-9999</Text></Text>
+            <Text>Hobby: <Text style={styles.txtInfo}>Programar</Text></Text>
+            </View>
+        ):(<View/>)}
+        { info === 'pedro' ? (
+           <View style={styles.containerInfo}>
+            <Text>Nome: <Text style={styles.txtInfo}>Pedro</Text></Text>
+            <Text>Idade: <Text style={styles.txtInfo}>25</Text></Text>
+            <Text>Profissão: <Text style={styles.txtInfo}>Padeiro</Text></Text>
+            <Text>Endereço: <Text style={styles.txtInfo}>Rua Com Saida, 591</Text></Text>
+            <Text>Numero: <Text style={styles.txtInfo}>21 99999-9999</Text></Text>
+            <Text>Hobby: <Text style={styles.txtInfo}>Exercitar-se</Text></Text>
+            </View>
+        ):(<View/>)}
+        { info === 'patrick' ? (
+           <View style={styles.containerInfo}>
+            <Text>Nome: <Text style={styles.txtInfo}>Patrick</Text></Text>
+            <Text>Idade: <Text style={styles.txtInfo}>30</Text></Text>
+            <Text>Profissão: <Text style={styles.txtInfo}>Padre</Text></Text>
+            <Text>Endereço: <Text style={styles.txtInfo}>Rua Almeida Torres, 23</Text></Text>
+            <Text>Numero: <Text style={styles.txtInfo}>21 99999-9999</Text></Text>
+            <Text>Hobby: <Text style={styles.txtInfo}>Ler Livros</Text></Text>
+            </View>
+        ):(<View/>)}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    containerApp: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+        textAlign: 'center'
+
+    },
+    containerInfo: {
+        width: '90%',
+        backgroundColor: '#FFF',
+        borderRadius: 20,
+        padding: 20,
+    },
+})
